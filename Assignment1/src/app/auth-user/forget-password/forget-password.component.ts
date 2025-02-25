f2d772e86c6e58cd81ca4f77f990278d803af282
smitsudani555 <smitsudani555@gmail.com>
2025-02-25 16:23:29 +0530

    assignment 1 - 2203031080016

---
+++ Assignment1/src/app/auth-user/forget-password/forget-password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forget-password',
+  imports: [ForgetPasswordComponent],
+  templateUrl: './forget-password.component.html',
+  styleUrl: './forget-password.component.css'
+})
+export class ForgetPasswordComponent {
+
+}
---
+++ Assignment1/src/app/auth-user/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [RegisterComponent],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
