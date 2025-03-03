65f1271b209934b16ec89df033de12135c59d87b
smitsudani555 <smitsudani555@gmail.com>
2025-03-03 11:17:28 +0530

    routing done

--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
+import { RegisterComponent } from './auth-user/register/register.component'
+import { UserComponent } from './auth-user/user/user.component'
+import { ForgetPasswordComponent } from './auth-user/forget-password/forget-password.component'
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path : 'login',
+        component : LoginComponent
+    },
+    {
+        path : 'register',
+        component : RegisterComponent
+    },
+    {
+        path : 'user',
+        component : UserComponent
+    },
+    {
+        path : 'forget-password',
+        component : ForgetPasswordComponent
+    },
+];
